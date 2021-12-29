9e8e662a8d897f80ddd1935584c824ed2bb80469
Josh <hosua.o@gmail.com>
2021-12-29 13:34:47 -0500

    stuff

--- get-dev-tools.bat
+++ get-dev-tools.bat
@@ -1,11 +1,11 @@
 @echo Press enter to start getting dev-tools
 @echo Before running, make sure winget (app installer) is installed from the Microsoft store.
-@echo Please also make sure you're running this in administrator mode, or PATHs won't be added!
+@echo Make sure you're running this in administrator mode, or oaths won't be added.
+
 pause
 
 @echo Installing Firefox
 cmd /c winget install -e --id Mozilla.Firefox
-
 @echo Opening firefox plugin URLs
 
 "C:\Program Files\Mozilla Firefox\firefox" "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/"
@@ -26,12 +26,12 @@ cmd /c winget install -e --id msys2.msys2
 @echo Installing vim
 cmd /c winget install -e --id vim.vim
 
-@echo Adding vim and msys2 to PATH
-cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files\Vim\vim82;C:\Program Files (x86)\Vim\vim82;"
+@echo Adding vim, vscode and msys2 to PATH
+cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files\Vim\vim82;C:\Program Files (x86)\Vim\vim82;C:\Users\Hos\AppData\Local\Programs\Microsoft VS Code;"
 
 @echo Installation complete, before closing this 
-@echo 1) Open MSYS2 MSYS 
-@echo 2) pacman -S --needed base-devel mingw-w64-x86_64-toolchain
+@echo Open MSYS2 MSYS, then copy & paste this command in. 
+@echo pacman -S --needed base-devel mingw-w64-x86_64-toolchain
 @echo After this, everything should be working.
 pause
 
