3b69124f497377a9e27bb370cbf511e97444b24c
Josh <hosua.o@gmail.com>
2021-12-29 11:40:31 -0500

    does it work

--- get-dev-tools.bat
+++ get-dev-tools.bat
@@ -2,6 +2,7 @@
 @echo Before running, make sure winget (app installer) is installed from the Microsoft store.
 @echo Please also make sure you're running this in administrator mode, or PATHs won't be added!
 pause
+
 @echo Installing Firefox
 cmd /c winget install -e --id Mozilla.Firefox
 @echo Installing Git
@@ -18,7 +19,13 @@ cmd /c winget install -e --id msys2.msys2
 cmd /c winget install -e --id vim.vim
 
 @echo Adding vim and msys2 to PATH
-cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files (x86)\Vim\vim82"
-@echo All done!
+cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files\Vim\vim82;C:\Program Files (x86)\Vim\vim82;"
+
+@echo Installation complete, before closing this 
+@echo 1) Open MSYS2 MSYS
+@echo 2) pacman -Syu
+@echo 3) Open MSYS2 MSYS again
+@echo 4) pacman -S --needed base-devel mingw-w64-x86_64-toolchain
+@echo After this, everything should be working.
 pause
 
