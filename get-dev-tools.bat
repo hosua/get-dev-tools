1d1b2af5f9c72b6ed5121fafee517dcf531f9989
Josh <hosua.o@gmail.com>
2022-01-19 18:18:59 -0500

    Update get-dev-tools.bat

--- get-dev-tools.bat
+++ get-dev-tools.bat
@@ -30,7 +30,7 @@ cmd /c winget install -e --id msys2.msys2
 cmd /c winget install -e --id vim.vim
 
 @echo Adding vim, vscode and msys2 to PATH
-cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files\Vim\vim82;C:\Program Files (x86)\Vim\vim82;"
+cmd /c setx /M path "%path%;C:\msys64\mingw64\bin;C:\Program Files\Vim\vim82;C:\Program Files (x86)\Vim\vim82;%USERPROFILE%\AppData\Local\Programs\Microsoft VS Code;"
 
 @echo Deleting unwanted shortcuts
 del %PUBLIC%\Desktop\*Edge*
